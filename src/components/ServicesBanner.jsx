8c1411e3652accbbcb96ab807dc77b1184f43d68
mcdaveGbenga <mcdavegbenga@gmail.com>
2025-03-29 18:12:41 +0100

    This is the latest Update

--- src/components/ServicesBanner.jsx
+++ src/components/ServicesBanner.jsx
@@ -8,7 +8,7 @@ const ServicesBanner = () => {
           <h1 className="text-4xl md:text-5xl font-bold text-white mb-4" data-aos="fade-up">
             Our Services
           </h1>
-          <div className="bg-red-800 width-[400px] h-[60px]"></div>
+         
           <div className="w-24 h-1 bg-yellow-400 mx-auto mb-6" data-aos="fade-up" data-aos-delay="100"></div>
           <p className="text-xl text-white/90 max-w-3xl mx-auto" data-aos="fade-up" data-aos-delay="200">
             Comprehensive solutions tailored to help your business grow and succeed in the digital world
